2a1b4f7220ef3e1a8810a9560068f76f9820864c
yangruiping11 <18435205183@163.com>
2021-05-26 15:07:00 +0800

    添加了文件夹

--- pythonstudy/testexecute/test_python.py
+++ pythonstudy/testexecute/test_python.py
@@ -6,5 +6,8 @@ def test_01():
 def test_02():
     assert  11 == 23
 
+def test_03():
+    assert 'HelloWorld' == 'HelloWorld'
+
 if __name__ == "__main__":
     pytest.main(["-s", "test_python.py"])
