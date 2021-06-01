b5106204498b23f9c5b268bc744c2dd0f8d7f635
yangruiping11 <18435205183@163.com>
2021-06-01 16:34:36 +0800

    提交基本的封装

---
+++ pythonstudy/utils/MyDB.py
@@ -0,0 +1,46 @@
+import pymysql
+from utils.ReadConfig import *
+
+class MyDB:
+    # from config read params
+    global host, port, user, password, database
+    host = ReadConfig.get_host()
+    port = int(ReadConfig.get_port())
+    user = ReadConfig.get_user()
+    password = ReadConfig.get_password()
+    database = ReadConfig.get_database()
+
+    def __init__(self):
+        self.db=None
+        self.cursor = None
+
+    '''
+    open database 
+    '''
+    def connectDB(self):
+        try:
+            #connection database
+            self.db = pymysql.connect(host=host, port=port, user=user, password=password, database=database)
+            #create cursor
+            self.cursor = self.db.cursor()
+            print("connection database successful")
+        except ConnectionError as ex:
+            print(ex)
+
+    def execute_sql(self,sql,params):
+         self.connectDB()
+         self.cursor.execute(sql,params)
+         self.db.commit()
+         return self.cursor
+
+    def get_all(self, cursor):
+        value = cursor.fetchall()
+        return value
+
+    def get_one(self, cursor):
+        value = cursor.fetchone()
+        return value
+
+    def closeDB(self):
+        self.db.close()
+        print("Database closed!")
---
+++ pythonstudy/utils/ReadRequest.py
@@ -0,0 +1,61 @@
+import requests
+import json
+'''
+封装 requests请求中的get与post方法。
+'''
+class ReadRequest:
+
+    #封装post请求
+    def post(self,url,data,headers):
+        global resp
+        if headers =='form-data':
+            resp = requests.post(url,data=data,headers=headers)
+        elif headers == "Content-type:application/json":
+            resp = requests.post(url,json=json.dumps(data),headers=headers)
+
+        return resp
+
+    #封装put请求
+    def put(self,url,data,headers):
+        global resp
+        if headers =='form-data':
+            resp = requests.put(url,data=data,headers=headers)
+        elif headers == "Content-type:application/json":
+            resp = requests.put(url,json=json.dumps(data),headers=headers)
+
+        return resp
+
+    #封装get请求
+    def get(self,url,data,headers):
+        global resp
+        if headers !=None:
+            resp = requests.get(url,data,headers)
+        else:
+            resp = requests.get(url,data)
+
+        return resp
+
+    def delete(self,url,data,headers):
+        resp = requests.delete(url)
+        return resp
+
+    #对封装的请求进行分方法的请求。
+    def run(self,method,url,data,headers):
+        global resp
+        if method == 'get':
+            resp = self.get(url,data,headers)
+        elif method == 'post':
+            resp = self.post(url,data,headers)
+        elif method == 'put':
+            resp = self.put(url,data,headers)
+        elif method == 'delete':
+            resp = self.delete(url,data,headers)
+        else:
+            print("the request method is error")
+        return resp
+
+if __name__ == '__main__':
+    resp = ReadRequest().run(method='get',url='https://reqres.in/api/users/2',data=None,headers=None)
+    print(resp.status_code)
+    print(json.dumps(resp.text))
+    print(resp.cookies)
